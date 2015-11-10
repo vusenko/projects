a37c5df89f967b0d26dc314433dbe0fb1c16ea7f
Vova <vova@thinkpad-latptop>
2015-11-11 01:37:04 +0200

    Finished Solution3(added second one via array).Updated unit tests

--- src/main/java/com/solution/Solution3.java
+++ src/main/java/com/solution/Solution3.java
@@ -2,12 +2,15 @@ package com.solution;
 
 import java.util.Stack;
 
+/**
+ * Given a string, find the length of the longest substring without repeating characters.
+ * For example, the longest substring without repeating letters for "abcabcbb" is "abc", which the length is 3.
+ * For "bbbbb" the longest substring is "b", with the length of 1.
+ */
 public class Solution3 {
 
     /**
-     * Given a string, find the length of the longest substring without repeating characters.
-     * For example, the longest substring without repeating letters for "abcabcbb" is "abc", which the length is 3.
-     * For "bbbbb" the longest substring is "b", with the length of 1.
+     * First solution for this issue via stack
      */
     public static int lengthOfLongestSubstring(String s) {
 
@@ -15,9 +18,7 @@ public class Solution3 {
         if(s.length() == 1) return 1;
 
         Stack<Character> subChars = new Stack<Character>();
-
         char [] chars = s.toCharArray();
-        int [] indices  = new int[chars.length];
 
         int index = 0;
         for(int i = 0; i< chars.length;i++){
@@ -25,29 +26,88 @@ public class Solution3 {
             if(!subChars.contains(chars[i])){
                 subChars.push(chars[i]);
 
-                //case when all unique
+                //case when all unique or last element
                 if(i == chars.length-1){
-                    return subChars.size();
+                    return (subChars.size() > index) ? subChars.size() : index;
                 }
 
-            }else {
-                indices[index] = subChars.size();
-                subChars.clear();
+            } else {
+                if(subChars.size() > index) index = subChars.size();
+                findAndRemove(subChars,chars[i]);
                 subChars.push(chars[i]);
-                ++index;
             }
         }
 
-        return getMax(indices);
+        return index;
     }
 
-    private static int getMax(int [] values){
-        int max = 0;
-        for(int i = 0 ; i <values.length; i++){
-            if(values[i] > max){
-                max = values[i];
+    private static void findAndRemove(Stack stack,char element){
+        int index = stack.indexOf(element);
+
+        if(index == 0){
+            stack.remove(index);
+            return;
+        }
+
+        for(int i = 0;i<=index;i++){
+            stack.remove(0);
+        }
+    }
+
+    /**
+     * Second solution for this issue via arrays
+     */
+    public static int lengthOfLongestSubstring2(String s) {
+
+        if(s.trim().isEmpty()) return 0;
+        if(s.trim().length() == 1) return 1;
+
+        char [] chars = s.toCharArray();
+        char [] subChars = new char [chars.length];
+
+        int index = 0;
+        int subCharsIndex = 0;
+        for(int i = 0 ; i < chars.length;i++){
+            if(getIndex(subChars,chars[i],subCharsIndex) == -1){
+
+                subChars[subCharsIndex] = chars [i];
+                ++subCharsIndex;
+
+                //case when all unique or last element
+                if(i == chars.length - 1){
+                    return (subCharsIndex > index) ? subCharsIndex : index;
+                }
+
+            } else {
+                if(subCharsIndex > index) index = subCharsIndex;
+                int indexOfDuplicate = getIndex(subChars,chars[i],subCharsIndex)+1;
+                findAndRemove(subChars,indexOfDuplicate,subCharsIndex);
+                subCharsIndex = subCharsIndex - indexOfDuplicate;//count
+                subChars[subCharsIndex] = chars[i];
+                ++subCharsIndex;
             }
         }
-        return max;
+        return index;
+    }
+
+    private static int findAndRemove (char [] chars,int index,int indexOfFullness){
+        if(indexOfFullness == 0){
+            indexOfFullness = chars.length;
+        }
+
+        for(int i = 0 ;i <index; i++ ){
+            for(int j = 0; j < indexOfFullness;j++){
+                chars [j] = chars[j+1];
+            }
+        }
+        return index;
+    }
+
+
+    private static int getIndex(char [] chars,char element,int indexOfFullness){
+        for(int i = 0; i < indexOfFullness;i++){
+            if(chars[i] == element) return i;
+        }
+        return -1;
     }
 }
--- src/test/java/com/solution/Solution3Test.java
+++ src/test/java/com/solution/Solution3Test.java
@@ -17,6 +17,9 @@ public class Solution3Test {
         String s5 = "abbbbb";
         String s6 = "bbbbbc";
         String s7= "abac";
+        String s8= "abba";
+        String s9 = "abbaabba";
+        String s10 = "ggububgvfk";
 
 
         assertEquals(0, Solution3.lengthOfLongestSubstring(s));
@@ -27,6 +30,23 @@ public class Solution3Test {
         assertEquals(2, Solution3.lengthOfLongestSubstring(s5));
         assertEquals(2, Solution3.lengthOfLongestSubstring(s6));
         assertEquals(3, Solution3.lengthOfLongestSubstring(s7));
+        assertEquals(2, Solution3.lengthOfLongestSubstring(s8));
+        assertEquals(2, Solution3.lengthOfLongestSubstring(s9));
+        assertEquals(6, Solution3.lengthOfLongestSubstring(s10));
+
+
+        assertEquals(Solution3.lengthOfLongestSubstring(s),Solution3.lengthOfLongestSubstring2(s));
+        assertEquals(Solution3.lengthOfLongestSubstring(s1),Solution3.lengthOfLongestSubstring2(s1));
+        assertEquals(Solution3.lengthOfLongestSubstring(s2),Solution3.lengthOfLongestSubstring2(s2));
+        assertEquals(Solution3.lengthOfLongestSubstring(s3),Solution3.lengthOfLongestSubstring2(s3));
+        assertEquals(Solution3.lengthOfLongestSubstring(s4),Solution3.lengthOfLongestSubstring2(s4));
+        assertEquals(Solution3.lengthOfLongestSubstring(s5),Solution3.lengthOfLongestSubstring2(s5));
+        assertEquals(Solution3.lengthOfLongestSubstring(s6),Solution3.lengthOfLongestSubstring2(s6));
+        assertEquals(Solution3.lengthOfLongestSubstring(s7),Solution3.lengthOfLongestSubstring2(s7));
+        assertEquals(Solution3.lengthOfLongestSubstring(s8),Solution3.lengthOfLongestSubstring2(s8));
+        assertEquals(Solution3.lengthOfLongestSubstring(s9),Solution3.lengthOfLongestSubstring2(s9));
+        assertEquals(Solution3.lengthOfLongestSubstring(s10),Solution3.lengthOfLongestSubstring2(s10));
+
     }
 
 }
